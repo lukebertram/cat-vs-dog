e100b2743d9e02412f6597f28239ed86b4ab21d6
Cat Janowitz and Luke Bertram <luke@>
2017-10-17 15:29:15 -0700

    add disappearing functionality to replies

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,25 @@ $(document).ready(function() {
   $("img#cat").click(function() {
     $("ul#cat-reply").append("<li>Pew-Pew-Pew-MEOW!</li>");
     $("ul#dog-reply").append("<li>Pew-Pew-Pew-BORK!</li>");
+    $("ul#cat-reply").children("li").last().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog-reply").children("li").last().click(function() {
+      $(this).remove();
+    });
   });
 
 
   $("img#dog").click(function() {
     $("ul#cat-reply").append("<li>Pew-Pew-Pew-MEOW!</li>");
     $("ul#dog-reply").append("<li>Pew-Pew-Pew-BORK!</li>");
+    $("ul#dog-reply").children("li").last().click(function() {
+      $(this).remove();
+    });
+    $("ul#cat-reply").children("li").last().click(function() {
+      $(this).remove();
+    });
   });
+
 
 });
